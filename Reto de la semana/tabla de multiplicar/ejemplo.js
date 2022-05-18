36705c0e6633d30ae7a63c3b935322006d058389
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2022-05-18 18:48:56 -0500

    Listo para la clase

---
+++ Reto de la semana/tabla de multiplicar/ejemplo.js
@@ -0,0 +1,13 @@
+let saludo= () => {
+    console.log("Hola a todos");
+}
+
+
+let onmouseoverTest =() =>{
+    console.log("Disparo onmouseover");
+}
+
+
+let onmouseoutTest =() =>{
+    console.log("Disparo onmouseout");
+}
