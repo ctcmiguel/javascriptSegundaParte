ebfe845bf06729ebbec56b1feed4314d7816e1b5
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2022-05-18 18:58:51 -0500

    localStorage

---
+++ temaLocalStorage/explicacion_01_DOM/ejemplo.js
@@ -0,0 +1,5 @@
+localStorage.setItem("lastname", "Smith");
+localStorage.clear;
+document.getElementById("demo").innerHTML = localStorage.getItem("lastname");
+localStorage.removeItem('lastname');
+localStorage.clear;
